2ed878c576f6137b7f6489c45a39c2a83d629b24
norsn <norsnSayt@gmail.com>
2022-02-08 08:28:24 +0900

    Fix Squash Task

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit2 <code>src/App.js</code> and3 save to reload4.
         </p>
         <a
           className="App-link"
